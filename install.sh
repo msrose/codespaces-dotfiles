4fe50249c2e9d8dc81dde8b816a490b0033e5ff2
Michael Rose <michael.rose@elevatesecurity.com>
2023-05-12 15:04:47 -0400

    Install more things

--- install.sh
+++ install.sh
@@ -5,5 +5,13 @@ git config --global alias.br branch
 git config --global alias.st status
 git config --global alias.ci commit
 
+sudo apt-get update
+sudo apt-get install vim
+sudo apt-get install tmux
+
 git clone https://github.com/msrose/dotvim ~/.vim
 bash ~/.vim/install.sh
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm install 16.17.0
